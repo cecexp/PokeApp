7c8de9435d8766bed08550813adc6e20075e1583
cecexp <trabajos.cece@gmail.com>
2024-06-27 21:23:42 -0600

    Design changes

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,17 +1,25 @@
+import React from "react";
 import { View, Text, FlatList } from "react-native";
+import { Card } from "react-native-paper";
 import { useFetchPokemonList } from "../hooks/PokeApi";
-
 const HomeScreen: React.FC = () => {
   const { pokemonList, loading } = useFetchPokemonList();
 
   if (loading) return <Text>Loading...</Text>;
 
   return (
-    <View>
-      <Text>Pokedex</Text>
+    <View style={{ flex: 1 }}>
+      <Text style={{ fontSize: 24, textAlign: "center", marginBottom: 10 }}>
+        Pokedex
+      </Text>
       <FlatList
         data={pokemonList}
-        renderItem={({ item }) => <Text key={item.name}>{item.name}</Text>}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => (
+          <Card style={{ margin: 12, padding: 12 }}>
+            <Text>{item.name}</Text>
+          </Card>
+        )}
       />
     </View>
   );
